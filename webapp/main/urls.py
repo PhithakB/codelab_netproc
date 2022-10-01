9239f6f6fcf5663ba1b377102ac000a009c1a264
PhithakB <phithakb2000s@gmail.com>
2022-10-02 02:20:06 +0700

    first commit

---
+++ webapp/main/urls.py
@@ -0,0 +1,13 @@
+#urls.py
+
+from django.urls import path
+from .views import (
+    list_view,
+    client
+)
+
+app_name = 'main'
+urlpatterns = [
+    path('', list_view, name='home-list'),
+    path('join-now',client, name='hangout'),#new url route
+]
